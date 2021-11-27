bb07f5c303cf01a60e8d11912a05447e46bdfc5c
SanatankrMishra <79195889+SanatankrMishra@users.noreply.github.com>
2021-11-27 11:58:49 +0530

    Update inserters.cpp

--- inserters.cpp
+++ inserters.cpp
@@ -1,52 +1,36 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-class box
-{
-    int x, y;
-
-public:
-    box(int i, int j)
-    {
-        x = i;
-        y = j;
-    }
-
-    friend ostream &operator<<(ostream &stream, box o);
+class phonebook{
+        char name[80];
+        int areacode;
+        int prefix;
+        int num;
+    public:
+        phonebook(char *n,int a,int p, int nm){
+            strcpy(name,n);
+            areacode=a;
+            prefix=p;
+            num=nm;
+        }
+        friend ostream &operator<<(ostream &stream, phonebook o);
 };
 
-ostream &operator<<(ostream &stream, box o)
-{
-    int i, j;
-    for (i = 0; i < o.x; i++)
-    {
-        stream << "*";
-        stream << "\n";
-        for (j = 1; j < o.y - 1; j++)
-        {
-            for (i = 0; i < o.x; i++)
-                if (i == 0 || i == o.x - 1)
-                    stream << "*";
-                else
-                    stream << " ";
-            stream << "\n";
-        }
-        for (i = 0; i < o.x; i++)
-            stream << "*";
-
-        stream << "\n";
-
-        return stream;
-    }
-
-    
+ostream &operator<<(ostream &stream, phonebook o){
+    stream<<o.name<<" ";
+    stream<<" (" <<o.areacode << ") ";
+    stream<<o.prefix<< "-"<< o.num << "\n";
+    return stream;
 }
 
-int main(int argc, char **argv)
-    {
+int main(){
 
-        box a(14, 6), b(30, 7), c(40, 5);
-        cout << a << b << c;
+    phonebook a("Ted", 111, 555, 1234);
+    phonebook b("Alice", 312, 555, 5768);
+    phonebook c("Tom", 212, 555,9991);
 
-        return 0;
-    }
+    cout<<a<<b<<c;
+
+    return 0;
+}
